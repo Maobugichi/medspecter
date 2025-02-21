d0b2be6c79446fb304d8875114e9295efa52f382
Maobugichi <immansamuel846@gmail.com>
2025-02-20 23:02:59 -0800

    second commit

--- src/actions.js
+++ src/actions.js
@@ -11,7 +11,7 @@ const navItem = [{
     text: "Overview",
     xmlns:"http://www.w3.org/2000/svg",
    viewBox:"0 0 24 24",
-   path:"/overviews/1"
+   path:"overviews/1"
 },
 
 {
--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     element: <Root/>,
     children: [
       {
-        path:"/overviews/:overviewId",
+        path:"overviews/:overviewId",
         element:<Overview/>
       }
     ]
