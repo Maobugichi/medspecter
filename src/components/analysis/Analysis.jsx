32f2c9dd6c8eb0e2d72440bfbac6ee7de9dccbb9
Maobugichi <immansamuel846@gmail.com>
2025-03-26 10:37:55 -0700

    remove APIKey

--- src/components/analysis/Analysis.jsx
+++ src/components/analysis/Analysis.jsx
@@ -6,26 +6,7 @@ import axios from "axios"
 import OpenAI from "openai";
 const Analysis = () => {
     const [ isShow , setIsShow ] = useState(false)
-    const apiEndpoint = 'https://api.openai.com/v1/completions';
-    const apiKey = import.meta.env.VITE_OPENAI_API_KEY
-    const client = new OpenAI({
-        apiKey:apiKey,
-        dangerouslyAllowBrowser: true,
-        maxRetries:0
-    })
-
-    async function  main() {
-        const chatCompletion = await client.chat.completions.create({
-            messages:[{role:"user",content:"say this is a test"}],
-            model:"gpt-4o"
-        })
-    }
-
-    useEffect(() => {
-        main()
-    },[])
-      
-      
+   
     return(
         <div className="flex lg:flex-row flex-col w-[95%] lg:w-[85%] mx-auto gap-5">
             <ImageAnalysis/>
