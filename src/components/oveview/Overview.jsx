314d1c0d0e0900245d50cd569bf5d56abefbed45
Maobugichi <immansamuel846@gmail.com>
2025-03-28 19:15:08 -0700

    still sorting responsiveness again2

--- src/components/oveview/Overview.jsx
+++ src/components/oveview/Overview.jsx
@@ -26,7 +26,7 @@ const Overview = () => {
 
   
     return(
-        <div className="xl:w-[85%] h-full xtraSize:bg-green-300 flex flex-col xl:flex-row gap-5 w-full md:pb-10 smallSize:bg-red-400">
+        <div className="xl:w-[85%] h-full  flex flex-col xl:flex-row gap-5 w-full md:pb-10 smallSize:bg-red-400 bg-green-600">
             <div className="xl:w-[65%] w-[95%]  h-auto min-h-[120vh] md:min-h-[80vh] lg:min-h-[130vh]   limitedSize:h-[80vh] flex flex-col mx-auto gap-5">
                 <h1 className="text-[24px] font-semibold">{greet} name</h1>
                 <div className="flex flex-col gap-10 xl:justify-between h-[85%]  limitedSize:h-[85%] ">
