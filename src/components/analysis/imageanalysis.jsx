ede7717b43b75a6b21bf87ba175927b2eb544d0e
Maobugichi <immansamuel846@gmail.com>
2025-03-31 11:41:13 -0700

    adjust pendingcases ://

--- src/components/analysis/imageanalysis.jsx
+++ src/components/analysis/imageanalysis.jsx
@@ -1,11 +1,12 @@
 import mri from "../../assets/mri.png";
+import Skeleton from 'react-loading-skeleton'
 
 const ImageAnalysis = () => {
     return(
         <div>
            
              <div className="lg:w-[95%] md:w-[95%]  mx-auto w-full  h-full">
-              <img className="w-full" src={mri} alt="MRI brain scan" />
+             {mri ? <img className="w-full" src={mri} alt="MRI brain scan" /> : <Skeleton/>}
             </div>
         </div>
        
--- src/components/pendingcases.jsx
+++ src/components/pendingcases.jsx
@@ -4,8 +4,8 @@ const PendingCases = ({id,name,diagnosis,date,status,bgColor,statusColor,statusT
         <div className={`${bgColor} flex h-[41px] text-[10px]  lg:text-[14px] w-full  text-[#000000] ${spacing} items-center`}>
             {id && <input className="w-2 md:w-10" type="checkbox" />}
             {id &&  <p className={`w-auto lg:min-w-[60px] flex  items-start `}>{id}</p>}
-             <p className={` md:w-auto md:min-w-[120px]  bg-red-200 whitespace-wrap flex items-start break-word w-[50px]`}>{name}</p>    
-             <p className={` flex items-start w-auto ${id ? "min-w-[50px] xxSmall:min-w-[40px] " : "min-w-[80px] lg:min-w-[100px] smallSize:min-w-[80px]"} md:min-w-[80px] whitespace-wrap  `}>{diagnosis}</p>
+             <p className={` md:w-auto md:min-w-[120px]   whitespace-wrap flex items-start break-word w-[50px]`}>{name}</p>    
+             <p className={` flex items-start w-auto ${id ? "min-w-[50px] xxSmall:min-w-[40px] " : "min-w-[70px] lg:min-w-[100px] smallSize:min-w-[80px]"} md:min-w-[80px] whitespace-wrap  bg-yellow-300`}>{diagnosis}</p>
              <p className={` flex items-start  w-auto min-w-[70px]  lg:min-w-[80px]`}>{date}</p>
             {treatment &&   <p className={`w-auto min-w-[70px] md:min-w-[100px] lg:min-w-[135px] whitespace-wrap   `}>{treatment}</p>}
             <p className={` grid place-content-center h-[23px] md:w-[100px] w-[80px] text-[9px] ${statusColor} ${statusText} rounded-2xl`}>{status}</p>
