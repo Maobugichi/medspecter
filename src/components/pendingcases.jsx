8603f70fe13c3d505b7bcc0b8c87351b2393265b
Maobugichi <immansamuel846@gmail.com>
2025-03-31 12:38:49 -0700

    check media query

--- src/components/pendingcases.jsx
+++ src/components/pendingcases.jsx
@@ -5,8 +5,8 @@ const PendingCases = ({id,name,diagnosis,date,status,bgColor,statusColor,statusT
             {id && <input className="w-2 md:w-10" type="checkbox" />}
             {id &&  <p className={`w-auto lg:min-w-[60px] flex  items-start `}>{id}</p>}
              <p className={` md:w-auto md:min-w-[120px]   whitespace-wrap flex items-start break-word w-[50px]`}>{name}</p>    
-             <p className={` flex items-start md:w-auto ${id ? "w-[50px] xxSmall:min-w-[40px] " : "w-[55px] lg:min-w-[100px] smallSize:min-w-[80px] break-word"} md:min-w-[80px] whitespace-wrap`}>{diagnosis}</p>
-             <p className={` flex items-start  w-auto min-w-[70px]  lg:min-w-[80px]`}>{date}</p>
+             <p className={` flex items-start md:w-auto ${id ? "w-[50px] xxSmall:min-w-[40px] " : "w-[55px] lg:min-w-[100px] smallSize:min-w-[80px] break-word"} md:min-w-[80px] whitespace-wrap bg-amber-200`}>{diagnosis}</p>
+             <p className={` flex items-start  w-auto min-w-[60px] bg-red-300  lg:min-w-[80px]`}>{date}</p>
             {treatment &&   <p className={`w-auto min-w-[70px] md:min-w-[100px] lg:min-w-[135px] whitespace-wrap   `}>{treatment}</p>}
             <p className={` grid place-content-center h-[23px] md:w-[100px] w-[80px] text-[9px] ${statusColor} ${statusText} rounded-2xl`}>{status}</p>
             {doctor && <p className={` flex items-start w-auto min-w-[100px]`}>{doctor}</p>}
