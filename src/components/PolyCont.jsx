24ce409da0cc09e6f6b869277ad9c31a06dde41c
Maobugichi <immansamuel846@gmail.com>
2025-04-03 02:54:13 -0700

    add svg animations ://

--- src/components/PolyCont.jsx
+++ src/components/PolyCont.jsx
@@ -1,7 +1,36 @@
 import PolyLine from "./aidiagnosis/PolyLine"
-import { motion } from "motion/react"
 import ProgressItem from "./aidiagnosis/ProgressItem"
+import { useEffect, useState } from "react"
 const PolyCont = ({width}) => {
+
+  const [ svgAttri , setSvgAttri ] = useState({
+    height:196,
+    width: 590
+  });
+  const [ windowWidth , setWindowWidth ] = useState(window.innerWidth);
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth)
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [window.innerWidth]);
+
+  useEffect(() => {
+    if (windowWidth < 600) {
+      setSvgAttri({
+        height:110,
+        width:310
+      })
+    }
+  },[windowWidth])
+ 
 
   const items = [
     {
@@ -20,7 +49,8 @@ const PolyCont = ({width}) => {
 
   const draw = {
     initial:{ pathLength: 0, strokeDasharray: 1000, strokeDashoffset: 1000, fillOpacity: 0 },
-    animate:{ pathLength: 1, strokeDashoffset: 0, fillOpacity: [0, 0.5 ,1] , transition: {default: {duration:5 , delay:0}, fillOpacity:{duration:2,delay : 5 , type: "tween"}} }
+    animate:{ pathLength: 1, strokeDashoffset: 0, fillOpacity: [0, 0.5 ,1] , transition: {default: {duration:5 , delay:0}, fillOpacity:{duration:2,delay : 5 , type: "tween"},  transform: svgAttri.transform // add this line
+  } }
 
 }
 
@@ -53,30 +83,11 @@ const PolyCont = ({width}) => {
               <li>40</li>
               <li>20</li>
             </ul>
-            <motion.svg  className=" absolute left-10 top-17" width="590" height="196" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
-            <g>
-            
-              <motion.path 
-              variants={draw}
-              initial="initial"
-              animate="animate"
-              d="m1,194.44c67.38,-20.39 97.7,-57.39 97.7,-57.39c0,0 44.88,-59.66 97.71,-72.61c52.83,-12.94 138.71,10.83 208.22,-8c69.51,-18.83 42.46,-48.74 113.88,-54.5c71.41,-5.75 87.26,54.64 144.62,34.8c57.36,-19.84 94.5,-18.27 94.5,-18.27" id="svg_1"
-              stroke="#9333EA" 
-              strokeWidth="2.33" 
-              
-              fill="none"/>
-              <motion.path 
-               variants={draw}
-               initial="initial"
-               animate="animate"
-               d="m0.9,176.31c82.75,1.57 127.54,-65.38 127.54,-65.38c0,0 18.56,-23.83 59.84,-32.88c41.27,-9.05 127.18,23.83 196.67,5c69.48,-18.83 59.48,-66.28 130.87,-72.03c71.38,-5.75 90.92,29.22 148.25,9.38c57.34,-19.84 94.46,-18.27 94.46,-18.27" id="svg_1" stroke="#3B82F6" stroke-width="2.33" fill="none"/>
-              <motion.path
-               variants={draw}
-               initial="initial"
-               animate="animate"
-              d="m0.9,161.26c67.53,-6.49 114.77,-59.05 195.05,-59.05c80.29,0 132.04,48.96 191.55,41.89c59.52,-7.07 73.68,-43.48 95.09,-68.97c21.41,-25.49 44.42,-46.67 85.84,-48.8c41.43,-2.12 59.5,18.34 90.78,10.88c31.89,-6.97 53.53,-35.76 99.32,-35.76" id="svg_1" stroke="#F59E0B" stroke-width="2.33" fill="none"/>
-            </g>
-            </motion.svg>
+           <PolyLine
+            svgAttri={svgAttri}
+            draw={draw}
+            windowWidth={windowWidth}
+           />
             <ul className="ml-8 flex w-[90%] justify-between text-[10px]">
               <li>Jan 29</li>
               <li>Jan 30</li>
--- src/components/aidiagnosis/PolyLine.jsx
+++ src/components/aidiagnosis/PolyLine.jsx
@@ -1,9 +1,35 @@
-const PolyLine = () => {
+import { motion } from "motion/react"
+
+const PolyLine = ({svgAttri, windowWidth , draw}) => {
     //0,290 30,280 40,275 90,240 120,210 140,190  170,170 220,145 250,135  300,135 370,150  458,160 550,140  580,140
     return(
-        <svg height={290} className=" w-[99%]" xmlns="https://www.w3.org/TR/SVG2/">
-            <polyline points="0,290 30,280 40,275 90,240 120,190 125,185 160,160 170,155 195,150 250,150 330,170 395,180 450,180 " style={{fill:"none",stroke:"red",strokeWidth:3}}/>
-        </svg>
+        <motion.svg  className=" absolute left-10 bottom-15 lg:top-17" width={svgAttri.width} height={svgAttri.height} xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+              <g>
+              
+                <motion.path 
+                variants={draw}
+                initial="initial"
+                animate="animate"
+                d="m1,194.44c67.38,-20.39 97.7,-57.39 97.7,-57.39c0,0 44.88,-59.66 97.71,-72.61c52.83,-12.94 138.71,10.83 208.22,-8c69.51,-18.83 42.46,-48.74 113.88,-54.5c71.41,-5.75 87.26,54.64 144.62,34.8c57.36,-19.84 94.5,-18.27 94.5,-18.27" id="svg_1"
+                stroke="#9333EA" 
+                strokeWidth="2.33" 
+                style={{ transform: windowWidth < 600 ?  'scale(0.4)' : 'scale(0.7)' }} 
+                fill="none"/>
+                <motion.path 
+                variants={draw}
+                initial="initial"
+                animate="animate"
+                d="m0.9,176.31c82.75,1.57 127.54,-65.38 127.54,-65.38c0,0 18.56,-23.83 59.84,-32.88c41.27,-9.05 127.18,23.83 196.67,5c69.48,-18.83 59.48,-66.28 130.87,-72.03c71.38,-5.75 90.92,29.22 148.25,9.38c57.34,-19.84 94.46,-18.27 94.46,-18.27" id="svg_1" stroke="#3B82F6" stroke-width="2.33" fill="none"
+                style={{ transform: windowWidth < 600 ?  'scale(0.4)' : 'scale(0.7)' }} 
+                />
+                <motion.path
+                variants={draw}
+                initial="initial"
+                animate="animate"
+                style={{ transform: windowWidth < 600 ?  'scale(0.4)' : 'scale(0.7)' }} 
+                d="m0.9,161.26c67.53,-6.49 114.77,-59.05 195.05,-59.05c80.29,0 132.04,48.96 191.55,41.89c59.52,-7.07 73.68,-43.48 95.09,-68.97c21.41,-25.49 44.42,-46.67 85.84,-48.8c41.43,-2.12 59.5,18.34 90.78,10.88c31.89,-6.97 53.53,-35.76 99.32,-35.76" id="svg_1" stroke="#F59E0B" stroke-width="2.33" fill="none"/>
+              </g>
+            </motion.svg>
     )
 }
 
